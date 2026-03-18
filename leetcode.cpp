49d4f5d1a51dc0d73fffb8fb86125b8c521cb0b7
Jason <498802124@qq.com>
2026-03-18 23:12:22 +0800

    add gitignore .vscode

--- leetcode.cpp
+++ leetcode.cpp
@@ -4,8 +4,16 @@
 #include "leetcode.h"
 
 
+#define CATCH_CONFIG_MAIN 1
+
 int main()
 {
     std::cout << "Hello CMake." << std::endl;
+    std::cout << "ca: " << CATCH_CONFIG_MAIN << std::endl;
+    #define CATCH_CONFIG_MAIN 2
+    std::cout << "ca2: " << CATCH_CONFIG_MAIN << std::endl;
+    double a = 0.1;
+    double* p = &a;
+    const int& b = a;
     return 0;
 }
